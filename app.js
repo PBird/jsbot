dd10b5c05861652585bf87c0bdeb026b2268368a
Sergen Sabahoglu <sergensabahoglu@gmail.com>
2018-10-03 22:32:25 +0300

    added strategy_4level

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ var BetController = function (strategy) {
 	const strategies = {
 		straight_2_6: strategy_2_6,
 		// straight_2_3: strategy_2_3,
-		// level_4_acc: strategy_4level
+		level_4_acc: strategy_4level
 	};
+
+	/*xxxxxxx______strategy_2_6_______xxxxxxxxxxx*/
 
 	function strategy_2_6() { // index kaçıncı tur olduğu; lastState ise son bette win mi lost mu
 		
@@ -77,8 +79,304 @@ var BetController = function (strategy) {
 		};
 
 
-
 	};
+
+	/*xxxxxxx_____strategy_4level_____xxxxxxxxxxx*/
+
+	function strategy_4level () {
+		const LEVELS = [1, 2, 4, 6];
+
+		const LEVEL_LIMITS = [7, 6, 5, 4];
+
+		
+		var levels_profits = [0, 0, 0, 0]; // profits
+
+
+		var indexOfLevel = -1;
+		var index = -1;
+		var streakOn = false;
+		var streakProfit = 0;
+		var times = 2;
+
+		function allReset () {
+			indexOfLevel = 0;
+			index = -1;
+			levels_profits = [0, 0, 0, 0];
+			streakProfit = 0;
+			streakOn = false;
+			streakProfit = 0;
+		}
+
+		function reset() {
+			indexOfLevel = 0;
+			index = 0;
+			levels_profits = [0, 0, 0, 0];
+			streakProfit = 0;
+			streakOn = false;
+			streakProfit = 0;
+
+
+			return LEVELS[indexOfLevel];
+		}
+
+		function win() {
+
+			if(streakOn){
+				index++;
+				streakProfit += LEVELS[indexOfLevel];
+				levels_profits[indexOfLevel] += LEVELS[indexOfLevel];
+
+				if(streakProfit >= 0){
+					index = 0;
+					streakOn = false;
+					streakProfit = 0;
+
+					if(indexOfLevel > 0){
+
+						if(	levels_profits[indexOfLevel] + levels_profits[indexOfLevel-1] > 0  ){ 
+							levels_profits[indexOfLevel] = 0;
+							indexOfLevel--;
+							levels_profits[indexOfLevel] = 0;
+							return LEVELS[indexOfLevel];
+						}else{
+							return LEVELS[indexOfLevel];
+						}
+
+					}else{
+						return LEVELS[indexOfLevel];
+					}
+				}else {
+
+					if(index > (LEVEL_LIMITS[indexOfLevel] -1)){
+
+						if (levels_profits[indexOfLevel] >= 0) {
+							index = 0;
+							streakOn = false;
+							streakProfit = 0;
+							return LEVELS[indexOfLevel];
+						}else {
+							indexOfLevel++;
+							if(indexOfLevel > (LEVELS.length-1)){
+								console.log('System over ');
+								return reset();
+							}else{
+								index = 0;
+								streakOn = false;
+								streakProfit = 0;
+								return LEVELS[indexOfLevel];
+							}
+							
+							
+						}
+
+					}else {
+						return LEVELS[indexOfLevel];
+					}
+
+					
+				}
+
+
+			}else{
+				if(indexOfLevel >0){
+					levels_profits[indexOfLevel] += LEVELS[indexOfLevel];
+
+					if(levels_profits[indexOfLevel] + levels_profits[indexOfLevel-1] > 0){ 
+						levels_profits[indexOfLevel] = 0;
+						indexOfLevel--;
+						levels_profits[indexOfLevel] = 0;
+						return LEVELS[indexOfLevel];
+					}else {
+						return LEVELS[indexOfLevel];
+					}
+
+
+				}else {
+					return LEVELS[indexOfLevel];
+				}
+			}
+
+
+			// index++;
+
+			// if(streakOn){ // Lost streak var ise
+				
+			// 	levels_profits[indexOfLevel] += LEVELS[indexOfLevel];
+			// 	streakProfit +=  LEVELS[indexOfLevel];
+
+			// 	if(levels_profits[indexOfLevel] >= 0){ // eğer win losta eşit veya fazlaysa  
+			// 		index = 0;
+			// 		expectedProfit =  indexOfLevel > 0  ? -levels_profits[indexOfLevel-1] : 0;
+			// 		if(levels_profits[indexOfLevel] > expectedProfit ){ // eğer önceki levelin zararını karşılamış ve biraz kar etmişse 
+			// 			if(indexOfLevel > 0 ){ // 0 levelden büyükse düşür.
+			// 				levels_profits[indexOfLevel] = 0;
+			// 				indexOfLevel--;
+			// 				levels_profits[indexOfLevel] = 0;
+			// 				return LEVELS[indexOfLevel];
+			// 			}else { // 0 ise  karı sıfırla
+			// 				levels_profits[indexOfLevel] = 0;
+			// 				return LEVELS[indexOfLevel];
+			// 			}
+						
+
+			// 		}else{
+			// 			if(expectedProfit == 0 && indexOfLevel)
+
+
+			// 		}
+
+
+			// 	}else{
+
+
+			// 	}
+
+			// }else{ // Lost streak yok
+			// 	index = 0;
+
+			// 	if(indexOfLevel> 0){ // lost streak yok ve level 0 değilse
+			// 		levels_profits[indexOfLevel] += LEVELS[indexOfLevel];
+				
+			// 		expectedProfit = -levels_profits[indexOfLevel-1];
+
+			// 		if(levels_profits[indexOfLevel] > expectedProfit ){
+			// 			levels_profits[indexOfLevel] = 0;
+			// 			indexOfLevel--;
+			// 			levels_profits[indexOfLevel] = 0;
+			// 			return LEVELS[indexOfLevel];
+
+			// 		}else{
+
+			// 			return LEVELS[indexOfLevel];
+
+			// 		}
+
+
+			// 	}else{ // lost streak yok ve level 0 sa
+			// 		return LEVELS[indexOfLevel];
+			// 	}
+			// }
+
+		
+			
+		};
+
+		function lost () {
+
+			streakOn = true;
+			streakProfit -= LEVELS[indexOfLevel];
+			levels_profits[indexOfLevel] -= LEVELS[indexOfLevel];
+			index++;
+
+			if( index > (LEVEL_LIMITS[indexOfLevel] - 1) ){
+
+				if(indexOfLevel > 0){
+					if(levels_profits[indexOfLevel] >= 0){
+						streakOn = false;
+						streakProfit = 0;
+						index = 0;
+
+						return LEVELS[indexOfLevel];
+
+					}else{
+						indexOfLevel++;
+						if(indexOfLevel > (LEVELS.length-1)){
+							console.log('System over ');
+							return reset();
+						}else{
+							streakOn = false;
+							streakProfit = 0;
+							index = 0;
+							return LEVELS[indexOfLevel];
+						}
+						
+					}
+
+				}else{
+					indexOfLevel++;
+					if(indexOfLevel > (LEVELS.length-1)){
+						console.log('System over ');
+						return reset();
+					}else{
+						streakOn = false;
+						streakProfit = 0;
+						index = 0;
+						return LEVELS[indexOfLevel];
+					}
+					
+				}
+
+			}else {
+				return LEVELS[indexOfLevel];
+			}
+
+
+			// streakOn = true;
+			// index++;
+			// levels_profits[indexOfLevel] -= LEVELS[indexOfLevel];
+
+			// if(index > LEVEL_LIMITS[indexOfLevel]){
+
+			// 	if(indexOfLevel > 0 && levels_profits[indexOfLevel] >= 0){
+
+			// 		index = 0;
+			// 		streakOn = false;
+			// 		return LEVELS[indexOfLevel];
+
+			// 	}else{
+
+			// 		indexOfLevel++;
+			// 		index = 0;
+			// 		return LEVELS[indexOfLevel];
+
+			// 	}
+
+			// }else{
+
+			// 	return LEVELS[indexOfLevel];
+
+			// }
+
+		}
+
+		function calcAmountOfBet(lastStatus) {
+			
+			if(index == -1){
+				return reset();
+			}else{
+				if(lastStatus == 1) // Win 
+				{
+					return win();
+
+				}else if(lastStatus == 0){ // lost
+					return lost();
+				}
+
+			}
+
+		
+		}
+
+		return {
+			getAmountOfBet: calcAmountOfBet, // parametre olarak son betin win mi lost mu olduğunu alır. win ise 1 , lost 0 , ilk ise  0 veya 1 den farklı değer
+			getTimes: function () {
+				 return times;
+			},
+			allReset: allReset,
+			testing: function () {
+				console.log('Level Profits : ', levels_profits);
+				console.log('streakOn : ', streakOn);
+				console.log('streakProfit: ', streakProfit);
+				console.log('indexOfLevel: ', indexOfLevel);
+				console.log('index: ', index);
+			}
+		};
+
+
+		
+	}
+
+
 
 	return {
 		Bettor: strategies[strategy](), 
@@ -162,6 +460,8 @@ var AppController = (function (UICtrl,BetCtrl) {
 		UICtrl.setBetValue(amount);
 		UICtrl.clickBtn(times);
 		console.log('Bet yapıldı');
+		console.log('------------info---------');
+		Bettor.testing();
 	}
 
 	function setupEventListener () {
@@ -191,7 +491,8 @@ var AppController = (function (UICtrl,BetCtrl) {
 	return {
 		init: function (strategy) {
 			DOM = UICtrl.getDOMSTRINGS();
-			Bettor= BetCtrl('straight_2_6').Bettor;
+			// Bettor= BetCtrl('straight_2_6').Bettor;
+			Bettor= BetCtrl('level_4_acc').Bettor
 			setupEventListener();
 
 
@@ -214,7 +515,50 @@ var AppController = (function (UICtrl,BetCtrl) {
 		},
 		testing: function () {
 			return observer;
+		},
+		testSystem: function (stg,wallet,round) {
+
+			//level_4_acc  
+			//straight_2_6 
+			Bettor= BetCtrl(stg).Bettor
+			var results = [];
+			var trues = 0;
+			var falses = 0;
+			var rand = false;
+			var indexon = true;
+			var indexOver = 0;
+			var bets = [];
+			for(let i= 0; i< round;i++){
+				var amount = 10 * Bettor.getAmountOfBet(rand);
+				bets.push(amount);
+				// console.log('Amount: ', amount);
+				rand = Math.random() >= 0.5;
+				results.push(rand);
+				if(rand){
+					trues++;
+					wallet += amount;
+				}else{
+					falses++;
+					wallet -= amount;
+				}
+				// console.log('Wallet :', wallet);
+
+				if(wallet < 0 && indexon){
+					indexOver = i;
+					indexon = false;
+				}
+
+			}
+
+			Bettor.allReset();
+
+			//console.log('Wallet : ', wallet);
+
+
+			return [wallet,indexOver,results,bets];
+			
 		}
+
 
 	}
 })(UIController,BetController);
@@ -234,6 +578,22 @@ observer = new MutationObserver(function(){
 		});
 
 observer.observe(document.getElementById('hash'),  {  childList: true });
+
+
+
+var bt = new BetController('level_4_acc').Bettor;
+
+
+var win=0, loss = 0; 
+for(var i = 0; i< 1000; i++){
+	var a = AppController.testSystem('straight_2_6',2040,10000);
+	if (a >= 2040){
+		win++;
+	}
+	else{
+    	loss ++;	
+	}
+};
 
 */
 
